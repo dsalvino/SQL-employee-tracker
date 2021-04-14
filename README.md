b11088dafc7b8fbfb80bc86802236276f6c955ae
denis salvino <denis.salvino23@gmail.com>
2021-04-14 03:24:59 -0500

    committing to update repository, I haven't tested if the code works LOL

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# SQL-employee-tracker
+ 
+# SQL Employee Tracker
+## About
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)<br>
+This project was created entirely out of backend to keep track of a companies employees.
+
+
+### Table of Contents
+**[About](#about)**<br>
+**[Installation Instructions](#installation-instructions)**<br>
+**[Usage Information](#usage-information)**<br>
+**[Test Instructions](#test-instructions)**<br>
+**[Contribution Guidelines](#contribution-guidelines)**<br>
+**[Licenses](#licenses)**<br>
+**[Contact Me](#contact-me)**<br>
+
+## Installation Instructions
+To install this project, you would do a git clone.
+## Usage Information
+The usage of this project is to keep track of your employees as a small company to add, update, and delete employees, roles, departments.
+## Test Instructions
+to test this project, you would npm install all necessary dependencies and run the server using node.
+## Contribution Guidelines
+To contribute to this project you would clone or fork.
+## Licenses
+MIT
+## Contact Me
+denis.salvino23@gmail.com<br>
+<https://github.com/dsalvino>
--- server.js
+++ server.js
@@ -18,25 +18,48 @@ const start = () => {
             message: 'What would you like to do?',
             choices: [
                 'Show all Employees',
-                'Show all Employees by Department',
-                'Show all Employees by  Role',
+                'Show Departments',
+                'Show Roles',
                 new inquirer.Separator(),
                 'Add new Department',
                 'Add new Role',
                 'Add new Employee',
                 new inquirer.Separator(),
-                'Update Employee roles'
+                'Update Employee roles',
+                'Quit'
             ],
         })
+        .then((response) => {
+            switch (response.prompt) {
+                case 'Show all Employees':
+                    return showEmployees(); 
+                case 'Show Departments':
+                    return showDepartment(); 
+                case 'Show Roles':
+                    return showRole(); 
+                case 'Add New Department':
+                    return addDepartment(); 
+                // "Remove Employee", 
+                case 'Add new Role':
+                    return addRole(); 
+                // "Update Employee Manager", 
+                case 'Add new Employee':
+                    return addEmployee();
+                case 'Update Employee roles': 
+                    return updateEmployee();
+                case 'Quit':
+                    return connection.end();
+            }
+        })
 }
-
+//shows
 const showEmployees = () => {
     connection.query('SELECT * FROM company_employees', (err, results) => {
         error ? console.error(err) : console.table(results);
         start();
     });
 };
-//todo
+
 const showDepartment = () => {
     connection.query('SELECT * FROM department', (err, results) => {
         error ? console.error(err) : console.table(results);
@@ -50,3 +73,128 @@ const showRole = () => {
         start();
     });
 };
+const selectRole = () => {
+    let roleArray = [];
+    connection.query('SELECT * FROM company_roles', (err, results) => {
+        err ? console.error(err) : results.forEach(role => {
+            roleArray.push(role)
+        });
+    });
+    return roleArray;
+};
+
+const selectManager = () => {
+    let managerArray = [];
+    connection.query('SELECT first_name, last_name FROM company_employees WHERE manager_id IS NULL', (err, results) => {
+        err ? console.error(err) : results.forEach(manager => {
+            managerArray.push(manager)
+        });
+    });
+    return managerArray;
+};
+
+//adds
+const addEmployee = () => {
+    inquirer.prompt([{
+        name: 'first',
+        messsage: "What is the employee's first name?",
+    }, {
+        name: 'last',
+        message: "What is the employee's last name?",
+    }, {
+        name: 'role',
+        message: "What is the employee's role?",
+        choices: selectRole()
+    }, {
+        name: 'manager',
+        message: "Who is this employee's Manager?",
+        choices: selectManager()
+    }
+    ])
+        .then(function (val) {
+            let roleID = selectRole().indexOf(val.role) + 1
+            let managerID = selectManager().indexOf(val.manager) + 1
+            connection.query('INSERT INTO company_employees SET ?',
+                {
+                    first_name: val.first,
+                    last_name: val.last,
+                    role_id: roleID,
+                    manager_id: managerID
+                }, (err) => {
+                    err ? console.error(err) : console.table(val);
+                    showEmployees();
+                })
+        })
+}
+
+const addRole = () => {
+    inquirer
+        .prompt([{
+            name: 'roleName',
+            message: 'What is the Role?'
+        }, {
+            name: 'salary',
+            message: 'What is the Salary of this Role?'
+        }])
+        .then(({ roleName, salary }) => {
+            connection.query('INSERT  INTO company_roles SET ?',
+                { roleName, salary }, (err, results) => {
+                    err ? console.error(err) : console.table(results);
+                    showRole();
+                })
+        })
+}
+
+const addDepartment = () => {
+    inquirer
+        .prompt([{
+            name: 'newDepartment',
+            message: 'What is the name of the new Department?'
+        }
+        ])
+        .then(({ newDepartment }) => {
+            connection.query('INSERT INTO department SET ?',
+                { newDepartment }, (err, results) => {
+                    err ? console.error(err) : console.log('New Department Added!');
+                    showDepartment();
+                })
+        })
+}
+
+const updateEmployee = () => {
+    connection.query('SELECT company_employees.id, company_roles.title FROM company_employees JOIN company_roles ON company_roles.role_id = role.id', (err, results) => {
+        err ? console.error(err) : console.table(results);
+        inquirer
+            .prompt([{
+                name: 'identification',
+                message: 'Enter your Employee ID',
+                choices: () => {
+                    let id = [];
+                    results.forEach(persons => {
+                        id.push(persons.id);
+                        return id;
+                    });
+                }
+            }, {
+                name: 'newRole',
+                type: 'list',
+                message: "What is the employee's new Role?",
+                choices: selectRole()
+            }])
+            .then((results) => {
+                let roleID = selectRole().indexOf(results.newRole) + 1
+                connection.query('UPDAATE company_employees SET WHERE ?',
+                    {
+                        id: res.identification
+                    }, {
+                    role_id: roleID
+                },
+                    (err) => {
+                        err ? console.error(err) : console.table(results);
+                        start();
+                    })
+
+            })
+    })
+}
+
